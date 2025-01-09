4400d5079eb17b6fee6ce855233756743f959059
digitalprobr <digitalprobr1@gmail.com>
2025-01-08 21:27:51 -0300

    Add files via upload

---
+++ modules/Videoai/database/seeders/VideoAiDatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\VideoAi\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Videoai\App\Models\PromptPreset;
+use Modules\VideoAi\Database\Seeders\PromptPresetSeeder;
+class VideoAiDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       
+    }
+}
